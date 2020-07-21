bc8d71ef82cf2f233f66b38027ff04cbda3a2dea
Donizete Gomes <donizetejr16@gmail.com>
2020-07-20 21:25:46 -0300

    capitulo 7 componentes e desafio componentes

---
+++ componentes/componente-intro/src/App.vue
@@ -0,0 +1,14 @@
+<template name="component-name">
+  <div id="app">    
+    <app-contadores />
+    <button>?</button>
+  </div>
+</template>
+<script>
+// JS
+export default {
+  
+}
+</script>
+<style>
+</style>
---
+++ componentes/componente-intro/src/Contadores.vue
@@ -0,0 +1,15 @@
+<template name="Component-name">
+    <div class="contadores">
+        <h1>Contadores</h1>
+        <app-contador v-form="c in 5" :key="c" />
+    </div>
+</template>
+<script>
+import Contador from './Contador.vue'
+export default {
+   components: { 'app-contador': Contador }
+}
+</script>
+<style>
+
+</style>
